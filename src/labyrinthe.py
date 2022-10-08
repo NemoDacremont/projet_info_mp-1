98b5f5df424a2bf5f2f4740a3d1f69f053055a3f
odasta <67148727+odasta@users.noreply.github.com>
2022-10-08 14:52:33 +0200

    Create labyrinthe.py

---
+++ src/labyrinthe.py
@@ -0,0 +1,12 @@
+
+
+
+def generate(n: int, p: int) -> list[int]:
+    # on travaillera plutôt avec des graphes après réflexion
+    length = n * p
+    out = [i for i in range(length)]
+
+    return out
+
+
+
