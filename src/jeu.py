b8ce0f68c25d26f4a21e502d432adc533d9f0cfe
Nemo D'acremont <nemo.dacremont@gmail.com>
2022-10-19 12:10:09 +0200

    Auto stash before merge of "main" and "origin/main"

--- src/jeu.py
+++ src/jeu.py
@@ -124,11 +124,11 @@ def update(game):
 	metAJourMinotaure(game, minotaure, joueur)
 
 	metAJourObjets(objets, game)
-	
+
 	game["mouvement"] += 1
-	
-	if game["mouvement"] >= game["vitesseSpectre"] :
-		game["spectre"]["vitesse"] += 1
+
+	if game["mouvement"] >= game["nouveauSpectre"] :
+		game["spectre"].append(creeSpectre(labyrinthe, joueur))
 		game["mouvement"] = 0
 
 	brouillardEstPersistant = joueur["brouillardEstPersistant"]
@@ -165,11 +165,12 @@ def affichage(game):
 
 		afficheBordure(labyrinthe)
 		afficheLabryinthe(labyrinthe, brouillard, utiliseBrouillard)
-		
-		afficheSpectre(spectre, brouillard, utiliseBrouillard)
+
+		for sp in spectre :
+			afficheSpectre(sp, brouillard, utiliseBrouillard)
 		afficheMinotaure(minotaure, brouillard, utiliseBrouillard)
-		
-		
+
+
 
 
 		## Affiche le joueur
